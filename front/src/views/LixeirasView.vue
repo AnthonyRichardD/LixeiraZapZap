c8dda7e0ac54fbdb14128fc24e3fd772170db951
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-06-12 20:08:48 -0300

    tela das lixeiras

---
+++ front/src/views/LixeirasView.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import DeviceView from '../components/DeviceView.vue';
+</script>
+
+<template>
+  <h1>Lixeiras</h1>
+  <DeviceView />
+</template>
+
+<style scoped>
+
+
+</style>
