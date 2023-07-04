2a349a64560b2f6f20a2bd1ba2f1bd237c32503e
Anthony Richard <106710005+aR1ch4rdz@users.noreply.github.com>
2023-07-04 09:59:35 -0300

    Update subscriber.js

--- mqtt-nodejs/cli/subscriber.js
+++ mqtt-nodejs/cli/subscriber.js
@@ -4,6 +4,16 @@ import { sendMessage } from './telegram.js';
 const client = mqtt.connect('https://broker.hivemq.com:1883'); // Substitua 'localhost' pelo endereço do seu broker MQTT, se necessário
 const topics = ['Devices/Device1','Devices/Device2']
 
+function getTime(){
+    let data = new Date();  
+    let hora = data.getHours();
+    let min = data.getMinutes();
+    let sec = data.getSeconds();
+  
+  return = `${hora}:${min}:${sec}`
+
+}
+
 client.on('connect', () => {
   console.log('Conectado ao broker MQTT');
   client.subscribe(topics, (err) => {
@@ -17,13 +27,14 @@ client.on('connect', () => {
 
 // chegada de mensagens
 client.on('message', (topic, message) => {
-  //console.log(`Mensagem no topico [${topic}]: ${message}`);
-  let device = JSON.parse(message)
+  let device = JSON.parse(message);
+  device.horario = getTime();
   console.log(device.volume);
   if(device.volume <= 10){
     sendMessage(`A lixeira localizada no ${device.local} atingiu sua capacidade máxima por volta de ${device.horario} e precisa ser esvaziada o mais rápido possível.`);
   } 
-  setDeviceData(JSON.parse(message))
+  setDeviceData(device)
+  console.log(`Mensagem no topico [${topic}]: ${message}`);
 });
 
 // Lidando com a desconexão
